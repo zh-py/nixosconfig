a6e2db817018898439cc4cf0e0c1b4711ad6c65d
zh-py <pierrez1984@gmail.com>
2024-03-01 08:41:36 +0300

    3.1

--- configuration.nix
+++ configuration.nix
@@ -181,10 +181,21 @@ in
     };
   };
 
-  environment.etc."wireplumber/bluetooth.lua.d/50-bluez-config.lua" = {
-    text = ''
+  #environment.etc."wireplumber/bluetooth.lua.d/50-bluez-config.lua" = {
+    #text = ''
+      #bluez_monitor.properties = {
+      #}
+    #'';
+  #};
+
+  environment.etc = {
+    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
       bluez_monitor.properties = {
         ["bluez5.default.rate"] = 44100,
+        ["bluez5.enable-sbc-xq"] = true,
+        ["bluez5.enable-msbc"] = true,
+        ["bluez5.enable-hw-volume"] = true,
+        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
       }
     '';
   };
