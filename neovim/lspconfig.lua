5ac1d839fbc90ce30c02ea17566e06b9a46f670b
zh-py <pierrez1984@gmail.com>
2024-03-03 15:49:31 +0300

    3.3

--- neovim/lspconfig.lua
+++ neovim/lspconfig.lua
@@ -1,5 +1,8 @@
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require('lspconfig')
+
+vim.lsp.set_log_level("debug")
+
 lspconfig.nil_ls.setup {
 	capabilities = lsp_capabilities,
 	on_attach = on_attach,
