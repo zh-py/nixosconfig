11b2b612deba102ecbecbd4a77778bed1d459b01
zh-py <pierrez1984@gmail.com>
2024-08-21 10:23:31 +0300

    8.20

--- home.nix
+++ home.nix
@@ -445,8 +445,9 @@ in {
       tab_bar_edge top
       tab_bar_style powerline
       tab_switch_strategy right
-      #tab_title_template " {index}: {f'{title[:6]}…{title[-6:]}' if title.rindex(title[-1]) + 1 > 13 else title.center(7)} "
+      #tab_title_template " {index}: {f'{title[:6]}…{title[-6:]}' if title.rindex(title[-1]) + 1 > 13 else title.center(7)}"
       tab_title_template "{index}: {title[title.rfind('/')+1:]}"
+      active_tab_font_style bold
       tab_bar_margin_width 6
       tab_powerline_style round
       tab_separator " ┇"
